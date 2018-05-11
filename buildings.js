1f9870e031a79ed9e816938cb10b063d170794e8
guydols <guydols@gmail.com>
2018-05-11 15:27:16 +0200

    Added tower functionality.

---
+++ buildings.js
@@ -0,0 +1,34 @@
+var func = {
+
+
+  //**************
+  // spawn screeps in rooms where they are lacking
+  towerJobs: function(tower) {
+    if(tower) {
+      var closestDamagedStructure = tower.pos.findClosestByRange(FIND_STRUCTURES, {
+        filter: (structure) => structure.hits < structure.hitsMax});
+      if(closestDamagedStructure) {
+        tower.repair(closestDamagedStructure);
+      }
+      var closestHostile = tower.pos.findClosestByRange(FIND_HOSTILE_CREEPS);
+      if(closestHostile) {
+        tower.attack(closestHostile);
+      }
+    }
+  },
+
+
+  //**************
+  //
+  y: function() {
+
+  },
+
+
+  //**************
+  //
+  z: function() {
+
+  }
+}
+module.exports = func;
--- functions.js
+++ functions.js
@@ -18,8 +18,9 @@ var func = {
     var ownedRooms = [];
     var ownedSpawns = [];
     var ownedCreeps = {};
+    var population = [2,1,5,3,0];
     var creepTiers = [[WORK,CARRY,MOVE,MOVE],
-                      [WORK,WORK,CARRY,CARRY,CARRY,MOVE,MOVE]];
+                      [WORK,WORK,CARRY,CARRY,CARRY,CARRY,MOVE,MOVE]];
 
     // register owned rooms and spawns
     for(let room in Game.rooms) {
@@ -71,25 +72,30 @@ var func = {
       for (let spawn in spawns) {
         if (spawns[spawn].spawning == null){
           for (let room in ownedCreeps) {
-            if (ownedCreeps[room]['harvesters'] < 2) {
-              spawns[spawn].spawnCreep([WORK,CARRY,MOVE,MOVE],
-                "Harvester" + Game.time.toString(),{memory: {r:0,o:room,s:0}});
+            if (ownedCreeps[room]['harvesters'] < population[0]) {
+              spawns[spawn].spawnCreep(creepTiers[1],
+                "Harvester" + Game.time.toString(),{memory: {r:0,o:room,s:0},
+                directions:[BOTTOM_RIGHT,BOTTOM,BOTTOM_LEFT]});
             }
-            if (ownedCreeps[room]['builders'] < 2) {
+            if (ownedCreeps[room]['builders'] <  population[1]) {
               spawns[spawn].spawnCreep([WORK,CARRY,MOVE,MOVE],
-                "Builder" + Game.time.toString(),{memory: {r:1,o:room,s:0}});
+                "Builder" + Game.time.toString(),{memory: {r:1,o:room,s:0},
+                directions:[BOTTOM_RIGHT,BOTTOM,BOTTOM_LEFT]});
             }
-            if (ownedCreeps[room]['upgraders'] < 3) {
+            if (ownedCreeps[room]['upgraders'] <  population[2]) {
               spawns[spawn].spawnCreep([WORK,CARRY,MOVE,MOVE],
-                "Upgrader" + Game.time.toString(),{memory: {r:2,o:room,s:0}});
+                "Upgrader" + Game.time.toString(),{memory: {r:2,o:room,s:0},
+                directions:[BOTTOM_RIGHT,BOTTOM,BOTTOM_LEFT]});
             }
-            if (ownedCreeps[room]['repairers'] < 2) {
+            if (ownedCreeps[room]['repairers'] <  population[3]) {
               spawns[spawn].spawnCreep([WORK,CARRY,MOVE,MOVE],
-                "Repairer" + Game.time.toString(),{memory: {r:3,o:room,s:0}});
+                "Repairer" + Game.time.toString(),{memory: {r:3,o:room,s:0,t:false},
+                directions:[BOTTOM_RIGHT,BOTTOM,BOTTOM_LEFT]});
             }
-            if (ownedCreeps[room]['haulers'] < 1) {
+            if (ownedCreeps[room]['haulers'] <  population[4]) {
               spawns[spawn].spawnCreep([CARRY,CARRY,CARRY,CARRY,CARRY,MOVE],
-                "Haulers" + Game.time.toString(),{memory: {r:4,o:room,s:0}});
+                "Haulers" + Game.time.toString(),{memory: {r:4,o:room,s:0},
+                directions:[BOTTOM_RIGHT,BOTTOM,BOTTOM_LEFT]});
             }
           }
         }
@@ -123,21 +129,17 @@ var func = {
 
 
   //**************
-  // tower fire at enemies
-  towerJob: function() {
-    var tower = Game.getObjectById('TOWER_ID');
-    if(tower) {
-      var closestDamagedStructure = tower.pos.findClosestByRange(FIND_STRUCTURES, {
-        filter: (structure) => structure.hits < structure.hitsMax});
-      if(closestDamagedStructure) {
-        tower.repair(closestDamagedStructure);
-      }
-
-      var closestHostile = tower.pos.findClosestByRange(FIND_HOSTILE_CREEPS);
-      if(closestHostile) {
-        tower.attack(closestHostile);
-      }
+  // get all towers and execute their jobs
+  towers: function(buildings) {
+    for(let room in Game.rooms) {
+      var towers = Game.rooms[room].find(FIND_MY_STRUCTURES,
+        {filter: (s) => s.structureType == STRUCTURE_TOWER});
+    }
+    for (let tower in towers) {
+      buildings.towerJobs(towers[tower]);
     }
   }
+
+  
 }
 module.exports = func;
