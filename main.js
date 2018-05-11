08805630a41daf45004ed673a097c59059365657
guydols <guydols@gmail.com>
2018-05-11 15:28:21 +0200

    Refractoring of repaierer roles and bits.

--- main.js
+++ main.js
@@ -1,11 +1,13 @@
 "use strict";
 const roles = require('./roles');
 const functions = require('./functions');
+const buildings = require('./buildings');
 
 module.exports.loop = function () {
 
-  functions.cleanup()
-  functions.spawning()
+  functions.cleanup();
+  functions.spawning();
   functions.execJobs(roles);
+  functions.towers(buildings);
 
 }
--- roles.js
+++ roles.js
@@ -89,27 +89,85 @@ var func = {
   //**************
   // repairer role
   repairer: function(creep){
-    if(creep.memory.s && creep.carry.energy == 0) {
-      creep.memory.s = false;
+    // states
+    // s == 0 get energy
+    // s == 1 get new targets
+    // s == 2 go repair target
+    // s == 9 init new creep
+    // t == 9 no target
+    // t != 9 target
+
+    // a new creep inits
+    if (creep.memory.s == 9) {
+      creep.memory.s = 0;
+      creep.memory.t = 9;
     }
-    if(!creep.memory.s && creep.carry.energy == creep.carryCapacity) {
-      creep.memory.s = true;
+    // check if energy is 0
+    if (creep.carry.energy == 0){
+      creep.memory.s = 0;
+      creep.memory.t = 9;
     }
-    if(creep.memory.s) {
-      var targets = creep.room.find(FIND_STRUCTURES, {filter: function(structure) {
-        if (structure.hits < structure.hitsMax){return structure;}}});
-        targets.sort((x,y) => x.hits - y.hits);
-        if(targets.length) {
-          if(creep.repair(targets[0]) == ERR_NOT_IN_RANGE) {
-            creep.moveTo(targets[0], {visualizePathStyle: {stroke: '#ffffff'}});
-          }
-        }
-    } else {
+    // get energy
+    if (creep.memory.s == 0) {
       var sources = creep.room.find(FIND_SOURCES);
-      if(creep.harvest(sources[1]) == ERR_NOT_IN_RANGE) {
-        creep.moveTo(sources[1], {visualizePathStyle: {stroke: '#ffaa00'}});
+      if(creep.harvest(sources[0]) == ERR_NOT_IN_RANGE) {
+        creep.moveTo(sources[0], {visualizePathStyle: {stroke: '#ffaa00'}});
+      }
+      if(creep.carry.energy == creep.carryCapacity){
+        creep.memory.s = 1;
       }
     }
+    // check if energy is not 0 and t is 9
+    if (creep.memory.s == 1 && creep.memory.t == 9){
+      var targets = creep.room.find(FIND_STRUCTURES, {filter: function(structure) {
+        if (structure.hits < structure.hitsMax){return structure;}
+      }});
+      targets.sort((x,y) => (((x.hits-x.hitsMax)/x.hitsMax)*100) - (((y.hits-y.hitsMax)/y.hitsMax)*100));
+      if(targets.length) {
+        creep.memory.t = targets[0].id;
+        creep.memory.s = 2;
+      }
+    }
+    // check if energy is not 0 and t is not 9
+    if (creep.memory.s == 2 && creep.memory.t != 9){
+      var target = Game.getObjectById(creep.memory.t);
+      // check if current target is fully healed
+      if (target.hits == target.hitsMax) {
+        creep.memory.t = 9;
+        creep.memory.s = 1;
+      }else {
+        if(creep.repair(target) == ERR_NOT_IN_RANGE) {
+          creep.moveTo(target, {visualizePathStyle: {stroke: '#ffffff'}});
+        }
+      }
+    }
+
+
+
+
+    // if(creep.memory.s && creep.carry.energy == 0) {
+    //   creep.memory.s = false;
+    // }
+    // if(!creep.memory.s && creep.carry.energy == creep.carryCapacity) {
+    //   creep.memory.s = true;
+    // }
+    // if(creep.memory.s) {
+    //   var targets = creep.room.find(FIND_STRUCTURES, {filter: function(structure) {
+    //   if (structure.hits < structure.hitsMax){return structure;}}});
+    //   targets.sort((x,y) => (((x.hits-x.hitsMax)/x.hitsMax)*100) - (((y.hits-y.hitsMax)/y.hitsMax)*100));
+    //   if(targets.length) {
+    //     if(creep.repair(targets[0]) == ERR_NOT_IN_RANGE) {
+    //       creep.moveTo(targets[0], {visualizePathStyle: {stroke: '#ffffff'}});
+    //     }
+    //   }
+    // } else {
+    //   var sources = creep.room.find(FIND_SOURCES);
+    //   if(creep.harvest(sources[0]) == ERR_NOT_IN_RANGE) {
+    //     creep.moveTo(sources[0], {visualizePathStyle: {stroke: '#ffaa00'}});
+    //   }
+    // }
+
+
   },
   //**************
   // hauler role
@@ -117,21 +175,20 @@ var func = {
     if(creep.memory.s && creep.carry.energy == 0) {
       creep.memory.s = false;
     }
-    console.log(creep.memory.s);
-    console.log(creep.carry.energy);
-    console.log(creep.carryCapacity);
+
     if(!creep.memory.s && creep.carry.energy == creep.carryCapacity) {
-      console.log("test");
       creep.memory.s = true;
     }
+
     if(creep.memory.s) {
       var targets = creep.room.find(FIND_STRUCTURES, { filter: (structure) => {
           return (structure.structureType == STRUCTURE_EXTENSION ||
                   structure.structureType == STRUCTURE_SPAWN ||
                   structure.structureType == STRUCTURE_TOWER) &&
-                  structure.energy < structure.energyCapacity;
+                  structure.esnergy < structure.energyCapacity;
         }
       });
+
         if(targets.length) {
           if(creep.transfer(targets[0]) == ERR_NOT_IN_RANGE) {
             creep.moveTo(targets[0], {visualizePathStyle: {stroke: '#ffffff'}});
